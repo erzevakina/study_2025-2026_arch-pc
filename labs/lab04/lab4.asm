977009928c915d18537ed1fc8a47683a238d2137
erzevakina <1032253564@pfur.ru>
2025-10-26 15:46:15 +0000

    feat(labs): add hello and lab4

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,17 @@
+; lab4.asm
+SECTION .data
+fio: DB 'Zevakina Ekaterina',19
+fioLen: EQU $-fio
+SECTION .text
+	GLOBAL _start
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,fio
+	mov edx,fioLen
+	int 80h
+	
+	
+	mov eax,1
+	mov ebx,0
+	int 80h
